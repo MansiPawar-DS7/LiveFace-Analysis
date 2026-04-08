68a98eac3813a3bb64e18a89c9ab43dc0587dc1b
Mansi <mansimpawar2006@gmail.com>
2026-04-08 16:04:07 +0530

    Refactor model paths to use BASE_DIR

--- comb_model.py
+++ comb_model.py
@@ -13,9 +13,11 @@ import gdown
 IMG_SIZE = 224
 CONFIDENCE_THRESHOLD = 0.25
 
-AGE_MODEL_PATH = r"C:\Users\Admin\Documents\TY_Project\training_age_model.h5"
-GENDER_MODEL_PATH = r"C:\Users\Admin\Documents\TY_Project\mobilenetv2_utkface_gender.h5"
-EMOTION_MODEL_PATH = r"C:\Users\Admin\Documents\TY_Project\best_emotion_model.h5"
+BASE_DIR = os.path.dirname(__file__)
+
+AGE_MODEL_PATH = os.path.join(BASE_DIR, "training_age_model.h5")
+GENDER_MODEL_PATH = os.path.join(BASE_DIR, "mobilenetv2_utkface_gender.h5")
+EMOTION_MODEL_PATH = os.path.join(BASE_DIR, "best_emotion_model.h5")
 
 if not os.path.exists(EMOTION_MODEL_PATH):
     url = "https://drive.google.com/uc?id=1sMqDb8zopHNa2m_Q3xNM84lQmmFZR1de"
